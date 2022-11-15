7ded1371cd6a913bd0c045d6c5b30b012c36ed8a
Arkady Schapov <sigma696@gmail.com>
2022-11-15 17:42:57 +0300

    fix errors after 2nd review

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const userRoutes = require('./routes/users');
 const cardRoutes = require('./routes/cards');
+const { RESOURCE_NOT_FOUND, RESOURCE_NOT_FOUND_MESSAGE } = require('./util/constants');
 
 const { PORT = 3000, MONGODB_URI = 'mongodb://localhost:27017/mestodb' } = process.env;
 const app = express();
@@ -19,7 +20,7 @@ app.use(express.json());
 app.use('/users', userRoutes);
 app.use('/cards', cardRoutes);
 app.use('*', (req, res) => {
-  res.status(404).send({ message: 'Неверный URL' });
+  res.status(RESOURCE_NOT_FOUND).send({ message: RESOURCE_NOT_FOUND_MESSAGE });
 });
 
 app.listen(PORT, () => {
--- util/constants.js
+++ util/constants.js
@@ -3,6 +3,7 @@ const GENERAL_ERROR_MESSAGE = 'Произошла ошибка';
 const BAD_REQUEST = 400;
 const BAD_REQUEST_MESSAGE = 'Переданы некорректные данные';
 const RESOURCE_NOT_FOUND = 404;
+const RESOURCE_NOT_FOUND_MESSAGE = 'Неверный URL';
 const USER_NOT_FOUND_MESSAGE = 'Пользователь по указанному ID не найден';
 const CARD_NOT_FOUND_MESSAGE = 'Карточка с указанным ID не найдена';
 const OK_CREATED = 201;
@@ -10,6 +11,7 @@ module.exports = {
   GENERAL_ERROR,
   GENERAL_ERROR_MESSAGE,
   RESOURCE_NOT_FOUND,
+  RESOURCE_NOT_FOUND_MESSAGE,
   USER_NOT_FOUND_MESSAGE,
   CARD_NOT_FOUND_MESSAGE,
   BAD_REQUEST,
