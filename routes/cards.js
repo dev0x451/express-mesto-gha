aa932f27d57bb148eed387c77b51f444edabc608
Arkady Schapov <sigma696@gmail.com>
2022-11-24 16:18:19 +0300

    fix errors 5

--- routes/cards.js
+++ routes/cards.js
@@ -5,6 +5,7 @@ const {
 } = require('../controllers/cards');
 
 router.get('/', getCards);
+
 router.post('/', celebrate({
   body: Joi.object().keys({
     name: Joi.string().required().min(2).max(30),
@@ -12,7 +13,26 @@ router.post('/', celebrate({
     link: Joi.string().required().pattern(/http(s)?:\/\/(www.)?[a-z0-9\.\-]+\/[a-z0-9\.\-_~:\/?#\[\]@!$&'()*+,;=]+/),
   }),
 }), createCard);
-router.delete('/:cardId', deleteCard);
-router.put('/:cardId/likes', setLike);
-router.delete('/:cardId/likes', removeLike);
+
+router.delete('/:cardId', celebrate({
+  params: Joi.object().keys({
+    cardId: Joi.string().required().min(24).max(24)
+      .pattern(/[a-z][0-9]+/),
+  }),
+}), deleteCard);
+
+router.put('/:cardId/likes', celebrate({
+  params: Joi.object().keys({
+    cardId: Joi.string().required().min(24).max(24)
+      .pattern(/[a-z][0-9]+/),
+  }),
+}), setLike);
+
+router.delete('/:cardId/likes', celebrate({
+  params: Joi.object().keys({
+    cardId: Joi.string().required().min(24).max(24)
+      .pattern(/[a-z][0-9]+/),
+  }),
+}), removeLike);
+
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,12 +1,28 @@
 const router = require('express').Router();
+const { celebrate, Joi } = require('celebrate');
 const {
   getUsers, getUserById, updateUser, updateAvatar,
 } = require('../controllers/users');
 
 router.get('/', getUsers);
 router.get('/me', getUserById);
-router.get('/:userId', getUserById);
-router.patch('/me', updateUser);
-router.patch('/me/avatar', updateAvatar);
+router.get('/:userId', celebrate({
+  params: Joi.object().keys({
+    userId: Joi.string().required().min(24).max(24)
+      .pattern(/[a-z][0-9]+/),
+  }),
+}), getUserById);
+router.patch('/me', celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().required().min(2).max(30),
+    about: Joi.string().required().min(2).max(30),
+  }),
+}), updateUser);
+router.patch('/me/avatar', celebrate({
+  body: Joi.object().keys({
+    // eslint-disable-next-line no-useless-escape
+    avatar: Joi.string().required().pattern(/http(s)?:\/\/(www.)?[a-z0-9\.\-]+\/[a-z0-9\.\-_~:\/?#\[\]@!$&'()*+,;=]+/),
+  }),
+}), updateAvatar);
 
 module.exports = router;
